1b8d76daa31e41c886845a2e3c2ede0e56172e17
Morgan Dunn <morgandunn21@gmail.com>
2020-12-28 12:29:28 -0600

    Added a basic map and tiles to use

--- Assets/ImportedAssets/HyperLuminal/2D Fantasy Forest Tileset/Scripts/CameraFollow.cs
+++ Assets/ImportedAssets/HyperLuminal/2D Fantasy Forest Tileset/Scripts/CameraFollow.cs
@@ -26,6 +26,11 @@ public class CameraFollow : MonoBehaviour
 	public GameObject PlayerCharacter;
 
 	/// <summary>
+	/// The speed the camera follows at
+	/// </summary>
+	public float cameraSpeed = 8.0f;
+
+	/// <summary>
 	///  Reference to the users current view transform.
 	/// </summary>
 	private Transform PlayerTransform;
@@ -39,36 +44,19 @@ public class CameraFollow : MonoBehaviour
 	
 	void Update ()
 	{
-		// By default the target x and y coordinates of the camera are it's current x and y coordinates.
-		Vector2 target = new Vector2(transform.position.x, transform.position.y);
 		
-		// If the player has moved beyond the x margin
-		if(CheckXMargin())
-		{
-			// the target X-coordinate should be a Lerp between the camera's current x position and the player's current x position.
-			target.x = Mathf.Lerp(transform.position.x, PlayerTransform.position.x , 8.0f * Time.deltaTime);
-		}
-		
-		// If the player has moved beyond the y margin
-		if(CheckYMargin())
-		{
-			// The target y coordinate should be a Lerp between the camera's current y position and the player's current y position.
-			target.y = Mathf.Lerp(transform.position.y, PlayerTransform.position.y , 8.0f * Time.deltaTime);
-		}
-		
-		// Clamp the camera within the bounds
-		target.x = Mathf.Clamp(target.x, MINBounds.x, MAXBounds.x);
-		target.y = Mathf.Clamp(target.y, MINBounds.y, MAXBounds.y);
-
-		// Set the camera's position to the target position with the same z component.
-		transform.position = new Vector3(target.x, target.y, transform.position.z);
 	}
 
-	/// <summary>
-	/// Checks if the distance between the camera and player (on X axis) is greater than X margin
-	/// </summary>
-	/// <returns><c>true</c>, is distance is greater, <c>false</c> otherwise.</returns>
-	bool CheckXMargin()
+    private void FixedUpdate()
+    {
+		MoveCamera(Time.fixedDeltaTime);
+	}
+
+    /// <summary>
+    /// Checks if the distance between the camera and player (on X axis) is greater than X margin
+    /// </summary>
+    /// <returns><c>true</c>, is distance is greater, <c>false</c> otherwise.</returns>
+    bool CheckXMargin()
 	{
 		// Returns true if the distance between the camera and the player in the x axis is greater than the x margin.
 		return Mathf.Abs(transform.position.x - PlayerTransform.position.x) > Margins.x;
@@ -82,5 +70,32 @@ public class CameraFollow : MonoBehaviour
 	{
 		// Returns true if the distance between the camera and the player in the y axis is greater than the y margin.
 		return Mathf.Abs(transform.position.y - PlayerTransform.position.y) > Margins.y;
+	}
+
+	private void MoveCamera(float timeDelta)
+    {
+		// By default the target x and y coordinates of the camera are it's current x and y coordinates.
+		Vector2 target = new Vector2(transform.position.x, transform.position.y);
+
+		// If the player has moved beyond the x margin
+		if (CheckXMargin())
+		{
+			// the target X-coordinate should be a Lerp between the camera's current x position and the player's current x position.
+			target.x = Mathf.Lerp(transform.position.x, PlayerTransform.position.x, cameraSpeed * timeDelta);
+		}
+
+		// If the player has moved beyond the y margin
+		if (CheckYMargin())
+		{
+			// The target y coordinate should be a Lerp between the camera's current y position and the player's current y position.
+			target.y = Mathf.Lerp(transform.position.y, PlayerTransform.position.y, cameraSpeed * timeDelta);
+		}
+
+		// Clamp the camera within the bounds
+		target.x = Mathf.Clamp(target.x, MINBounds.x, MAXBounds.x);
+		target.y = Mathf.Clamp(target.y, MINBounds.y, MAXBounds.y);
+
+		// Set the camera's position to the target position with the same z component.
+		transform.position = new Vector3(target.x, target.y, transform.position.z);
 	}
 }
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour 
+{
+	#region Member Variables
+	/// <summary>
+	/// Player movement speed
+	/// </summary>
+	public float movementSpeed;
+
+	/// <summary>
+	/// Animation state machine local reference
+	/// </summary>
+	private Animator animator;
+
+	private Rigidbody2D rb;
+
+	private Vector2 movement;
+	#endregion
+
+	// Use this for initialization
+	void Start ()
+	{
+		// get the local reference
+		animator = GetComponent<Animator>();
+
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//Get the player input
+		GetInput();
+		//Update the snimation state
+		UpdateAnimator();
+	}
+
+    private void FixedUpdate()
+    {
+		//Move the player
+		MovePlayer();
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+	{
+		
+	}
+
+	/// <summary>
+	/// Respawns the player at checkpoint.
+	/// </summary>
+	public void Respawn()
+	{
+	}
+
+	private void GetInput()
+    {
+		// check for player exiting the game
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
+
+		movement.x = Input.GetAxisRaw("Horizontal");
+		movement.y = Input.GetAxisRaw("Vertical");
+	}
+
+	private void MovePlayer()
+    {
+		rb.velocity = movement.normalized * movementSpeed;
+	}
+
+	private void UpdateAnimator()
+    {
+		
+	}
+
+}
