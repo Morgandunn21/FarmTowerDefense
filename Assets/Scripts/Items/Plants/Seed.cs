5889dca6ab2fea05bd02a3f9ac2af2c5c596d9b4
Morgan Dunn <morgandunn21@gmail.com>
2020-12-28 18:00:13 -0600

    Reworked Inventory to not be Monobehavior

---
+++ Assets/Scripts/Items/Plants/Seed.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Objects/Seed", order = 1)]
+public class Seed : Item
+{
+    public Plant plant;
+
+    public override void UseItem()
+    {
+        base.UseItem();
+    }
+}
